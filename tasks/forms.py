c05b2b3243697cb6b8c1b4c04177b5e3d4c98b07
myo <0233023e@gmail.com>
2022-11-02 10:04:08 -0500

    Submit styles for django crud

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,8 +1,13 @@
-from django.forms import ModelForm
+from django import forms
 from tasks.models import Task
 
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'important']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Write a title to task'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write a description to task'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input'})
+        }
